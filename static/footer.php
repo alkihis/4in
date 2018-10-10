fed9c28ed6e7a081de1d83592e616ecf8ee83dcf
wzamoum <wael.zamoum@etu.univ-lyon1.fr>
2018-10-10 16:28:57 +0200

    Footer

--- static/footer.php
+++ static/footer.php
@@ -4,33 +4,29 @@
             <div class="col l6 s12">
                 <h5 class="white-text">Company Bio</h5>
                 <p class="grey-text text-lighten-4">
-                    We are a team of college students working on this project like it's our full time job.
-                    Any amount would help support and continue development on this project and is greatly appreciated.
+                    Nous somme une équipe d'étudiants en master qui travaillons sans relache pour créer la meilleure base de données de gènes liés à l'immunité du monde.
                 </p>
             </div>
             <div class="col l3 s12">
-                <h5 class="white-text">Settings</h5>
+                <h5 class="white-text">Menu</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+                    <li><a class="white-text" href="#!">Accueil</a></li>
+                    <li><a class="white-text" href="#!">Rechercher</a></li>
+                    <li><a class="white-text" href="#!">Ajouter un gène</a></li>
+                    <li><a class="white-text" href="https://blast.ncbi.nlm.nih.gov/Blast.cgi">BLAST</a></li>
                 </ul>
             </div>
             <div class="col l3 s12">
-                <h5 class="white-text">Connect</h5>
+                <h5 class="white-text">Partenaires</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+                    <li><a class="white-text" href="http://www.inra.fr/">INRA</a></li>
+                    <li><a class="white-text" href="http://bf2i.insa-lyon.fr/">BF2i</a></li>
+                    <li><a class="white-text" href="https://www.universite-lyon.fr/">Université de Lyon</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
-        <div class="container">
-            Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-        </div>
+        <div class="container"></div>
     </div>
 </footer>
