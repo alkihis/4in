81da045de4b72707eb3e8a903ac30d635298627d
wzamoum <wael.zamoum@etu.univ-lyon1.fr>
2018-10-16 16:55:17 +0200

    Correction for help

--- pages/help.php
+++ pages/help.php
@@ -35,11 +35,32 @@ function helpView(Controller $c) : void {
                 <h5 class='title'>
                     Base de données
                 </h5>
-                <p class="light text-justify">
+                <p class="light text-justify flow-text">
                             La base de données contient des gènes provenant de 14 espèces d'insectes différentes. 
                             Chaque gène dispose d'un nom, d'un identifiant unique, d'une ou plusieurs voies immunitaires associées
                             ainsi que des gènes homologues si ils existent.
                             La base de données à été crée avec MySQL et est accessible via le site web, qui permet d'effectuer différentes requêtes.
+                </p>
+            </div>
+            <div class="section">
+                <h5 class='title'>
+                    Recherche
+                </h5>
+                <p class="light text-justify flow-text">
+                            Le site web permet d'effectuer des recherches variées sur l'ensemble ou non de la base de données. 
+                            Vous pouvez effectuer des recherches de 4 types : par nom, par identifiant, par voie ou recherche avancée qui vous permet de
+                            cumuler les paramètres pour faire une recherche spécifique.
+                </p>
+            </div>
+            <div class="section">
+                <h5 class='title'>
+                    BLAST
+                </h5>
+                <p class="light text-justify flow-text">
+                            Il est possible d'aligner des séquences importées depuis votre ordinateur sur l'ensemble ou une partie de la base de données en utilisant l'outil "BLAST".
+                            Il vous sera nécessaire d'avoir un fichier au format fasta ou bien de copier vos séquences directement dans la case associée.
+                            Le résultat de l'alignement sera présenté sous la forme d'une page web, contenant les résultats de l'alignement au format standard pour
+                            les différents gènes avec lesquels vous avez choisi d'aligner votre séquence.
                 </p>
             </div>
         </div>
