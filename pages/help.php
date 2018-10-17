8234c0c4828d14b441f82d4c23cff5192b026589
wzamoum <wael.zamoum@etu.univ-lyon1.fr>
2018-10-17 14:54:58 +0200

    help page -> help section

--- pages/help.php
+++ pages/help.php
@@ -1,23 +1,57 @@
 <?php 
 
 ////// HOME //////
-function helphHomePage() : array {
+function helpHomePage() : array {
+    
     return [];
 }
 
 function helpControl(array $args) : Controller {
+
     if (! isset($args[0])) {
-        $data = helphHomePage();
         $data['mode'] = "home";
+        $title = "Aide - Accueil";
+    }
+    else if ($args[0] === 'DB') {
+        $data['mode'] = "DB";
+        $title = "Aide - Database";
+    }
+    else if ($args[0] === 'search') {
+        $data['mode'] = "search";
+        $title = "Aide - Recherche";
+    }
+    else if ($args[0] === 'BLAST') {
+        $data['mode'] = "BLAST";
+        $title = "Aide - BLAST";
     }
     else {
         throw new PageNotFoundException();
     }
 
-    return new Controller($data, "Aide");
+    return new Controller($data, $title);
 }
 
 function helpView(Controller $c) : void {
+    $d = $c->getData();
+
+    switch ($d['mode']) {
+        case 'home':
+            showHelpHome($d);
+            break;
+        case 'DB':
+            showHelpDB($d);
+            break;
+        case 'search':
+            showHelpSearch($d);
+            break;
+        case 'BLAST':
+            showHelpBlast($d);
+            break;
+    }
+}
+
+////// HOME //////
+function showHelpHome(array $data) : void {
     ?>
     <div class="parallax-container parallax-search-page">
         <div class="parallax"><img src="/img/mol_search.jpg"></div>
@@ -31,39 +65,73 @@ function helpView(Controller $c) : void {
                     Rubrique Aide
                 </h4>
             </div>
-            <div class="section">
-                <h5 class='title'>
-                    Base de données
-                </h5>
-                <p class="light text-justify flow-text">
+            <div class="row no-margin-bottom">
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center">
+                            <a href='/help/DB'><i class="material-icons mat-title light-blue-text">settings_system_daydream</i></a>
+                        </h2>
+                        <h5 class="center">
+                            <a href='/help/DB' class='no-link-color'>Base de données</a>
+                        </h5>
+
+                        <p class="light text-justify">
                             La base de données contient des gènes provenant de 14 espèces d'insectes différentes. 
                             Chaque gène dispose d'un nom, d'un identifiant unique, d'une ou plusieurs voies immunitaires associées
                             ainsi que des gènes homologues si ils existent.
                             La base de données à été crée avec MySQL et est accessible via le site web, qui permet d'effectuer différentes requêtes.
-                </p>
-            </div>
-            <div class="section">
-                <h5 class='title'>
-                    Recherche
-                </h5>
-                <p class="light text-justify flow-text">
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center">
+                            <a href='/help/search'><i class="material-icons mat-title light-blue-text">search</i></a>
+                        </h2>
+                        <h5 class="center">
+                            <a href='/help/search' class='no-link-color'>Recherche</a>
+                        </h5>
+
+                        <p class="light text-justify">
                             Le site web permet d'effectuer des recherches variées sur l'ensemble ou non de la base de données. 
                             Vous pouvez effectuer des recherches de 4 types : par nom, par identifiant, par voie ou recherche avancée qui vous permet de
                             cumuler les paramètres pour faire une recherche spécifique.
-                </p>
-            </div>
-            <div class="section">
-                <h5 class='title'>
-                    BLAST
-                </h5>
-                <p class="light text-justify flow-text">
+                        </p>
+                    </div>
+                </div>
+                <div class="col s12 m4  ">
+                    <div class="icon-block">
+                        <h2 class="center">
+                            <a href='/help/BLAST'><i class="material-icons mat-title light-blue-text">sort</i></a>
+                        </h2>
+                        <h5 class="center">
+                            <a href='/help/BLAST' class='no-link-color'>BLAST</a>
+                        </h5>
+
+                        <p class="light text-justify">
                             Il est possible d'aligner des séquences importées depuis votre ordinateur sur l'ensemble ou une partie de la base de données en utilisant l'outil "BLAST".
                             Il vous sera nécessaire d'avoir un fichier au format fasta ou bien de copier vos séquences directement dans la case associée.
                             Le résultat de l'alignement sera présenté sous la forme d'une page web, contenant les résultats de l'alignement au format standard pour
                             les différents gènes avec lesquels vous avez choisi d'aligner votre séquence.
-                </p>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     <?php
+}
+
+////// DATABASE //////
+function showHelpDB(array $data) : void {
+
+}
+
+////// SEARCH //////
+function showHelpSearch(array $data) : void {
+}
+
+////// BLAST //////
+function showHelpBlast(array $data) : void {
 }
